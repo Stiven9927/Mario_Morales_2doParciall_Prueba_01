4a95b17ecb41d0cb113ef02d0b8de89d08029c3f
Stiven9927 <estelarmaximo@gmail.com>
2022-01-10 19:00:32 -0500

    3er Commit 19:00

--- app/src/main/java/deitel/com/mario_morales_2doparcial_prueba_01/msmm_MainActivity.java
+++ app/src/main/java/deitel/com/mario_morales_2doparcial_prueba_01/msmm_MainActivity.java
@@ -2,14 +2,36 @@ package deitel.com.mario_morales_2doparcial_prueba_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class msmm_MainActivity extends AppCompatActivity {
 
+    EditText user, pass;
+    Button aceptar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.msmm_activity_main);
+
+        user = findViewById(R.id.User_editText);
+        pass = findViewById(R.id.Pass_editText);
+        aceptar = findViewById(R.id.Acep_button);
+
+     aceptar.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+            if(user.getText().toString().equals("user") && pass.getText().toString().equals("123")){
+                //Intent intent = new Intent(this, );
+            }else{
+                Toast.makeText(msmm_MainActivity, "Credenciales Incorrectas", Toast.LENGTH_SHORT).show();
+             }
+         }
+     });
     }
-    private val;
 }
